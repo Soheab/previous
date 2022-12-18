d0da7691bbfbda2e607c7392e20656cff8cd9cb9
Soheab_ <33902984+Soheab@users.noreply.github.com>
2022-12-18 15:36:24 +0100

    Add closing help thread status
    Add ⏱️ Closing status to help threads to indicate that the thread is to be closed soon. I guess we can close threads with this status after 12/24 hours unless the author sends something useful.

--- cogs/help.py
+++ cogs/help.py
@@ -46,6 +46,7 @@ HELP_TOPIC_EMOJIS: Dict[str, str] = {
     "🤚": "Help Needed",
     "🔥": "Active",
     "⏳": "Stalled",
+    "⏱️": "Closing",
     "🛠️": "Development Version",
     "🐛": "Bug Report",
     "📌": "Pinned",
